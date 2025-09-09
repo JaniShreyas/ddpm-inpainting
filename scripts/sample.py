4d8f9d35278309316460cedba78d83b9cfe7f322
JaniShreyas <janishreyas5@gmail.com>
2025-09-09 13:54:25 +0530

    Move device to the top of config and fix saving logic

--- scripts/sample.py
+++ scripts/sample.py
@@ -1,11 +1,13 @@
 import torch
 from src.models.ddpm import DiffusionModel
 from torchvision.utils import save_image
+import os
 
 
 def main():
     # Read config [FIX]
     config = {
+        'device': 'cuda',
         "model": {
             "in_channels": 1,
             "out_channels": 1,
@@ -15,11 +17,11 @@ def main():
     }
 
     # Instantiate the model
-    model = DiffusionModel(model_config=config['model'])
+    model = DiffusionModel(model_config=config['model']).to(config['device'])
 
     # Load a trained checkpoint
-    checkpoint_path = "path/temporary/trained_model.pt"
-    model.load_state_dict(torch.load(checkpoint_path))
+    checkpoint_path = "checkpoints/default_exp/model_epoch_final.pt"
+    model.load_state_dict(torch.load(checkpoint_path, map_location=config['device']))
     model.eval()
 
     # Call the sample method
@@ -28,6 +30,7 @@ def main():
     # Save the images
     # Temporarily overwrites old saves / saves to same location. Need to change later [FIX]
     output_path = "output/samples/generated_grid.png"
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
     save_image(generated_images, output_path, nrow=4)
     print(f"Saved generated images to {output_path}")
 
