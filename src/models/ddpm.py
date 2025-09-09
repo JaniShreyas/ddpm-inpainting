d312ccf16a1ad8eb7979890a2f31c7aa780e4de9
JaniShreyas <janishreyas5@gmail.com>
2025-09-09 11:51:40 +0530

    Add the sampling logic to the ddpm

--- src/models/ddpm.py
+++ src/models/ddpm.py
@@ -4,6 +4,8 @@ import torch.nn.functional as F
 
 from .unet import UNet
 
+from tqdm import tqdm
+
 
 class DiffusionModel(nn.Module):
     def __init__(self, model_config):
@@ -18,6 +20,11 @@ class DiffusionModel(nn.Module):
 
         self.alphas = 1. - self.betas
         self.alphas_cumprod = torch.cumprod(self.alphas, dim=0)
+
+        # Just moves alphas_cumprod forward by 1, removes right most, and sets first value to 1
+        self.alphas_cumprod_prev = F.pad(self.alphas_cumprod[:-1], (1,0), value=1.0)
+        # The variance values needed during sampling
+        self.posterior_variance = self.betas * (1. - self.alphas_cumprod_prev) / (1. - self.alphas_cumprod)
 
     def q_sample(self, x_start, t, noise=None):
         if noise is None:
@@ -42,3 +49,38 @@ class DiffusionModel(nn.Module):
         # Calculate loss
         loss = F.mse_loss(noise, predicted_noise)
         return loss
+    
+    @torch.no_grad()
+    def sample(self, num_images, image_size, device='cuda'):
+        """
+        Inference method for generating new images
+        """
+        # Start with pure random noise
+        x = torch.randn(num_images, self.unet.in_channels, image_size, image_size, device=device)
+
+        # The reverse diffusion loop. Calculate and remove noise
+        for t in tqdm(reversed(range(0, len(self.betas))), desc="Sampling", total=len(self.betas)):
+            t_tensor = torch.full((num_images,), t, device=device, dtype=torch.long)
+
+            # Predict the noise from the UNet
+            predicted_noise = self.unet(x, t_tensor)
+
+            # The denoising formula to get a cleaner image. From the paper
+            alpha_t = self.alphas[t]
+            alpha_cumprod_t = self.alphas_cumprod[t]
+
+            beta_t = self.beta[t]
+
+            coeff = (1 - alpha_t) / (1 - alpha_cumprod_t).sqrt()
+            mean = (1 / alpha_t.sqrt()) * (x - coeff * predicted_noise)
+
+            if t > 0:
+                variance = self.posterior_variance[t].sqrt()
+                z = torch.randn_like(x)
+                x = mean + variance * z
+            else:
+                x = mean
+
+        # Denormalize images from [-1, 1] to [0, 1] for viewing and saving
+        x = (x.clamp(-1,1) + 1) / 2
+        return x
