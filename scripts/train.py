588e21e3ab372fb2571a32ff7dc0b5143cefc488
JaniShreyas <janishreyas5@gmail.com>
2025-09-10 09:44:33 +0530

    Implement MLFlow for experiment tracking and saving models and artifacts

--- scripts/train.py
+++ scripts/train.py
@@ -1,4 +1,5 @@
 import torch
+import mlflow
 
 # Replace with factory get_dataloaders to fit with config yaml
 from src.data.mnist import get_dataloaders
@@ -8,43 +9,67 @@ from src.training.trainer import Trainer
 from src.data.config import DataConfig
 
 
+def flatten_config(config):
+    """Flattens a nested dictionary for MLflow logging."""
+    flat_params = {}
+    for key, value in config.items():
+        if isinstance(value, dict):
+            for sub_key, sub_value in value.items():
+                flat_params[f"{key}.{sub_key}"] = sub_value
+        else:
+            flat_params[key] = value
+    return flat_params
+
 def main():
     # Read config
     # Temporary config setup here. Replace with Hydra config management [FIX]
     config = {
+        "experiment_name": "ddpm_mnist_base",
         "device": "cuda" if torch.cuda.is_available() else "cpu",
+        # "seed": 42, # Not yet implemented to use this. Random for now
         "dataset": {"name": "mnist", "batch_size": 128, "image_size": 32},
         "model": {
             "in_channels": 1,
             "out_channels": 1,
             "base_channels": 32,
             "channel_multipliers": (1, 2),
+            "time_emb_dim": 128
         },
-        "optimizer": {"lr": 1e-4},
         "training": {
-            "epochs": 10,
+            "epochs": 1,
+            "lr": 1e-4
         },
+        "sampling": {
+            "num_images": 16,
+            "sample_every_n_epochs": 5
+        }
     }
 
-    # Setup dataloaders
-    data_config = DataConfig(**config["dataset"])
-    train_dataloader, test_dataloader = get_dataloaders(data_config)
 
-    # Setup model
-    model = DiffusionModel(model_config=config["model"])
+    mlflow.set_experiment(config['experiment_name'])
 
-    # Setup optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=config["optimizer"]["lr"])
+    with mlflow.start_run():
+        mlflow.log_params(flatten_config(config))
 
-    # Instantiate and run the trainer
-    trainer = Trainer(
-        model=model,
-        dataloader=train_dataloader,
-        optimizer=optimizer,
-        device=config["device"] ** config["training"],
-    )
+        # Setup dataloaders
+        data_config = DataConfig(**config["dataset"])
+        train_dataloader, test_dataloader = get_dataloaders(data_config)
 
-    trainer.train()
+        # Setup model
+        model = DiffusionModel(model_config=config["model"])
+
+        # Setup optimizer
+        optimizer = torch.optim.Adam(model.parameters(), lr=config["training"]["lr"])
+
+        # Instantiate and run the trainer
+        trainer = Trainer(
+            model=model,
+            dataloader=train_dataloader,
+            optimizer=optimizer,
+            config=config
+        )
+
+        trainer.train()
 
 
 if __name__ == "__main__":
--- src/training/trainer.py
+++ src/training/trainer.py
@@ -1,15 +1,21 @@
 import torch
+import mlflow
 from tqdm import tqdm
 import os
+from torchvision.utils import save_image
 
 class Trainer:
-    def __init__(self, model, dataloader, optimizer, device, epochs, experiment_name="default_exp"):
-        self.model = model.to(device)
+    def __init__(self, model, dataloader, optimizer, config):
+
+        self.config = config
+        self.device = config["device"]
+        self.epochs = config["training"]["epochs"]
+        self.experiment_name = config["experiment_name"]
+        self.sample_every_n_epochs = config["sampling"]["sample_every_n_epochs"]
+
+        self.model = model.to(self.device)
         self.dataloader = dataloader
         self.optimizer = optimizer
-        self.device = device
-        self.epochs = epochs
-        self.experiment_name = experiment_name
 
     def _train_epoch(self, epoch_num):
         self.model.train()
@@ -39,17 +45,48 @@ class Trainer:
         
         avg_loss = total_loss / len(self.dataloader)
         print(f"Epoch {epoch_num} - Average loss: {avg_loss:.4f}")
+        
         # Log metrics for MLFlow
+        mlflow.log_metric("avg_loss", avg_loss, step=epoch_num)
 
-    def save_checkpoint(self, epoch_num):
-        save_path = f"checkpoints/{self.experiment_name}/model_epoch_{epoch_num}.pt"
 
-        # Ensure the directory exists
-        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    def _save_and_log_checkpoint(self, epoch_num, is_final=False):
+        temp_checkpoint_path = f"temp_checkpoint_epoch_{epoch_num}.pt"
+        torch.save(self.model.state_dict(), temp_checkpoint_path)
 
-        # Save the state dict
-        torch.save(self.model.state_dict(), save_path)
-        print(f"Checkpoint saved to {save_path}")
+        # Log as artifact
+        mlflow.log_artifact(temp_checkpoint_path, artifact_path="checkpoints")
+
+        # If it is final, save as mlflow model as well
+        if is_final:
+            mlflow.pytorch.log_model(self.model, name="model")
+            print("Final model saved in MLFlow model format")
+
+        # Clean up temporary file
+        os.remove(temp_checkpoint_path)
+        print(f"Logged checkpoints for epoch {epoch_num} to MLFlow")
+
+
+    def sample_and_log_images(self, epoch_num):
+        self.model.eval()
+        with torch.no_grad():
+            generated_images = self.model.sample(
+                num_images=self.config["sampling"]["num_images"],
+                image_size=self.config["dataset"]["image_size"],
+                device=self.device
+            )
+        
+        # Save temporary file
+        temp_path = f"temp_sample_epoch_{epoch_num}.png"
+        save_image(generated_images, temp_path, nrow=4)
+
+        # Log as an artifact to mlflow
+        mlflow.log_artifact(temp_path, artifact_path="samples")
+
+        # Clean up temporary file
+        os.remove(temp_path)
+        print(f"Logged sample images for epoch {epoch_num} to MLFlow")
+    
 
     def train(self):
         print("Starting training...")
@@ -57,9 +94,11 @@ class Trainer:
             self._train_epoch(epoch)
             
             # Save a checkpoint every 10 epochs (can use validation loss as a metric later)
-            if epoch % 10 == 0:
-                self.save_checkpoint(epoch)
+            if epoch % self.sample_every_n_epochs == 0:
+                self._save_and_log_checkpoint(epoch)
+                self.sample_and_log_images(epoch)
         
         # Always save the final model
-        self.save_checkpoint("final")
+        self._save_and_log_checkpoint("final", is_final=True)
+        self.sample_and_log_images("final")
         print("Training complete.")
