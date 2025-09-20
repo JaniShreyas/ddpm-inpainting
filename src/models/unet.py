ce9462671d072d1f23e95b957c2b3c3ca914a61b
JaniShreyas <janishreyas5@gmail.com>
2025-09-20 21:04:55 +0530

    Separate the building blocks from the Unet

--- src/models/unet.py
+++ src/models/unet.py
@@ -1,57 +1,7 @@
 import torch
 import torch.nn as nn
-import math
 
-
-class SinusoidalPositionEmbeddings(nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.dim = dim
-
-    def forward(self, time):
-        device = time.device
-        half_dim = self.dim // 2
-        embeddings = math.log(10_000) / (half_dim - 1)
-        embeddings = torch.exp(torch.arange(half_dim, dtype=torch.float32, device=device) * -embeddings)
-        embeddings = time[:, None] * embeddings[None, :]
-        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
-        return embeddings
-
-
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, time_emb_dim):
-        super().__init__()
-        self.time_mlp = nn.Linear(time_emb_dim, out_channels)
-        self.conv1 = nn.Conv2d(
-            in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1
-        )
-        self.conv2 = nn.Conv2d(
-            in_channels=out_channels,
-            out_channels=out_channels,
-            kernel_size=3,
-            padding=1,
-        )
-        # self.relu = nn.ReLU()
-        # self.batch_norm = nn.BatchNorm2d(out_channels)
-        self.norm1 = nn.GroupNorm(8, out_channels)
-        self.norm2 = nn.GroupNorm(8, out_channels)
-        self.act = nn.SiLU()
-        self.residual_conv = (
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1)
-            if in_channels != out_channels
-            else nn.Identity()
-        )
-
-    def forward(self, x, t):
-        # Here, t already comes after SinusoidalEmbedding + a linear layer (and relu).
-        # This time_mlp is used to change shape of the vector (pretty sure)
-        h = self.act(self.norm1(self.conv1(x)))
-        time_emb = self.act(self.time_mlp(t))
-        time_emb = time_emb.view(time_emb.shape[0], -1, 1, 1)
-        h += time_emb
-        h = self.act(self.norm2(self.conv2(h)))
-        return h + self.residual_conv(x)
-
+from .blocks import SinusoidalPositionEmbeddings, ResidualBlock
 
 class UNet(nn.Module):
     def __init__(
