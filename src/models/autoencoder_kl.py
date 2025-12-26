77b24dd0adc42378494e2c9c3911f25e86e094eb
JaniShreyas <janishreyas5@gmail.com>
2025-12-26 16:53:34 +0530

    Implement encoder init class

--- src/models/autoencoder_kl.py
+++ src/models/autoencoder_kl.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from src.models.utils.blocks import ResidualBlock, AttentionBlock
+
 # Should contain an Encoder and Decoder class with config as input
 
 
@@ -11,14 +13,68 @@ class Encoder(nn.Module):
         in_channels: int,
         out_channels: int,
         latent_channels: int,
+        double_z: bool,
         base_channels: int,
         channel_multipliers: list[int],
         attention_resolutions: list[int],
-        time_emb_dim: int,
         num_res_blocks: int,
         dropout: int,
+        image_size: int,
     ):
-        pass
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.base_channels = base_channels
+
+        self.num_res_blocks = num_res_blocks
+
+        self.image_size = image_size
+
+        if double_z:
+            self.latent_channels = 2 * latent_channels
+        else:
+            self.latent_channels = latent_channels
+
+
+        # Initial convolution to base_channels
+        self.initial_conv = nn.Conv2d(self.in_channels, self.base_channels, 3, padding=1)
+
+        channels = [self.base_channels] + [self.base_channels*channel_multipliers[i] for i in range(len(channel_multipliers))]
+        current_res = self.image_size
+
+        self.down = nn.Sequential()
+
+        # m blocks of (Residual + Downsample)
+        for i in range(len(channels) - 1):
+            in_ch = channels[i]
+            out_ch = channels[i+1]
+            
+            downs = []
+            for _ in range(self.num_res_blocks):
+                downs.append(ResidualBlock(in_ch, out_ch, dropout=dropout))
+                in_ch=out_ch
+            if current_res in attention_resolutions:
+                downs.append(AttentionBlock(out_ch))
+
+            downs.append(nn.Conv2d(out_ch, out_ch, kernel_size=4, stride=2, padding=1))
+            
+            current_res //= 2  # Since we always half the resolution
+
+            self.down.add_module(f"{i}", nn.Sequential(*downs))
+        
+        # Residual, Attention, Residual
+        self.bottleneck = nn.Sequential(
+            ResidualBlock(in_channels=channels[-1], out_channels=channels[-1], dropout=dropout),
+            AttentionBlock(channels[-1]),
+            ResidualBlock(in_channels=channels[-1], out_channels=channels[-1], dropout=dropout),
+        )
+
+        # GroupNorm, SiLU, Conv2D to latent_channels (will be given double z or not by the VAE class)
+        # 32 groups is a standard heuristic choice
+        self.group_norm = nn.GroupNorm(num_groups=32, num_channels=channels[-1], eps=1e-6, affine=True)
+        self.silu = nn.SiLU()
+        self.latent = nn.Conv2d(channels[-1], self.latent_channels, kernel_size=3, padding=1)
+
 
     def forward(self, x):
         pass
