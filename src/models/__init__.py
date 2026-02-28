fda9ac9f86a39c3e06bf696e00a158715f87ed0f
JaniShreyas <janishreyas5@gmail.com>
2026-02-28 23:28:43 +0530

    Add JiT classes from their repo and add it to factory. Still needs other fixes to fit in with standard.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,6 +2,7 @@ from src.models.backbones.unet import UNet
 from src.models.backbones.unet_attention import UNetWithAttention
 from src.models.ddpm import DiffusionModel
 from src.models.autoencoder_kl import AutoEncoderKL
+from src.models.backbones.jit import JiT
 
 import torch.nn as nn
 
@@ -21,11 +22,17 @@ def create_autoencoder_kl(config):
     model = AutoEncoderKL(config=config)
     return model
 
+def create_ddpm_jit(config):
+    backbone = JiT(**config.model.backbone)
+    model = DiffusionModel(backbone=backbone, config=config)
+    return model
+
 # Model Registry
 MODEL_REGISTRY = {
     "ddpm_unet_base": create_ddpm_unet_base,
     "ddpm_unet_attention": create_ddpm_unet_attention,
     "autoencoder_kl": create_autoencoder_kl,
+    "ddpm_jit": create_ddpm_jit,
 }
 
 def get_model(config) -> nn.Module:
